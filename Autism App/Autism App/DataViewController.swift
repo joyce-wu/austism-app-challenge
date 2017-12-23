8fc8a343815a0224c0b4ca66532b214bccc01905
gracecuenca <gcuenca@stuy.edu>
2017-12-23 14:00:30 -0500

    started xcode proj

---
+++ Autism App/Autism App/DataViewController.swift
@@ -0,0 +1,33 @@
+//
+//  DataViewController.swift
+//  Autism App
+//
+//  Created by Grace Cuenca on 12/23/17.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
