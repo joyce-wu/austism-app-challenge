7e448133f0a57cb0dedf5cb35a83d205f9cb891f
gracecuenca <gcuenca@stuy.edu>
2017-12-26 11:55:40 -0500

    chart testing

--- Autism App/Autism App/Charts.swift
+++ Autism App/Autism App/Charts.swift
@@ -5,7 +5,24 @@
 //  Created by Grace Cuenca on 12/25/17.
 //
 
-import Foundation
+import UIKit
 import Charts
+
+class ChartViewController: UIViewController, GetChartData {
+    
+}
+
+class BarChart: UIView {
+    
+    //bar chart properties
+    let barChartView = BarChartView()
+    var dataEnrty: [BarChartDataEntry] = []
+    
+    //chart data
+    var incidentDate = [String]()
+    var numIncidents = [String]()
+    
+    
+}
 
 
